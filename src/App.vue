369a182da5afc402c5708f7747189f47deef896a
Mannyg448 <manny.g448@hotmail.com>
2023-02-15 08:58:34 -0600

    adding the index and deletion with usestorage

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,43 @@
 <script setup>
-
+  import { ref } from 'vue'
+  import { useStorage } from '@vueuse/core'
+  import { nanoid } from 'nanoid'
+  import confetti from 'canvas-confetti'
+  const newGrocery = ref('')
+  const groceries = useStorage('groceries', [])
+  const addGrocery = () => {
+    if (newGrocery.value) {
+      groceries.value.push({ id: nanoid(), name: newGrocery.value })
+      newGrocery.value = ''
+    }
+  }
+  const deleteGrocery = id => {
+    const removeIndex = groceries.value.findIndex(grocery => grocery.id === id)
+    groceries.value.splice(removeIndex, 1)
+    confetti({ particleCount: 300, spread: 1000, origin: { y: 1 } })
+  }
 </script>
 
 <template>
   <main>
-  <h1 class="title">My Vue Grocery List ✎</h1>
-  <form class="newGroceryForm">
-        <input id="newGrocery" 
+    <h1 class="title">📝 Vue Grocery List 📝</h1>
+    <form class="newGroceryForm" @submit.prevent="addGrocery">
+      <input
+        id="newGrocery"
         autocomplete="off"
-        type="text" 
-        placeholder="Add an item to your list" />
-        <button id="addBtn" type="submit">Add</button>
+        type="text"
+        placeholder="Add an item to your list"
+        v-model="newGrocery"
+      />
+      <button type="submit">Add</button>
     </form>
-
+    <h3>Pending Items: {{ groceries.length }}</h3>
     <ul>
-      <li>Chips</li>
+      <li v-for="grocery in groceries" @click="deleteGrocery(grocery.id)">
+        {{ grocery.name }}
+      </li>
     </ul>
-</main>
+  </main>
 </template>
 
 <style lang="postcss" scoped>
